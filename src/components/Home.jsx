bddb7b35fd624e0a7d9d6cc80a0b10205b68772e
detumo <adetumo@gmail.com>
2025-01-03 23:46:48 +0100

    lesson 7 click event

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,20 @@
 
 const Home = () => {
+
+    const handleClick = () => {
+        console.log('hello, ninjas ')
+    }
+
+    const handleClickAgain = () => {
+        console.log(`hello $name`)
+    }
+
   return (
     <div>
-        <div className="content mx-w-[600px] my-10 mx-auto p-5">
+        <div className="home mx-w-[600px] my-10 mx-auto p-5">
             <h1>Homepage</h1>
+            <button onClick={handleClick}>click me</button>
+            <button onClick={() => handleClickAgain('mario')}>click me again</button>
         </div>
     </div>
   )
