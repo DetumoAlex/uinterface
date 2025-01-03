0c10523400e219448ebd77e2c4c9c5be28a4f588
detumo <adetumo@gmail.com>
2025-01-03 23:56:37 +0100

    useState -8

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,20 +1,26 @@
+import { useState } from "react"
 
 const Home = () => {
+    const [name, setName] = useState('alex')
+    const [age, setAge] = useState(25)
 
     const handleClick = () => {
-        console.log('hello, ninjas ')
+        // console.log('hello, ninjas ')
+        setName('detumo')
+        setAge(30)
     }
 
-    const handleClickAgain = () => {
-        console.log(`hello $name`)
-    }
+    // const handleClickAgain = () => {
+    //     console.log(`hello $name`)
+    // }
 
   return (
     <div>
         <div className="home mx-w-[600px] my-10 mx-auto p-5">
             <h1>Homepage</h1>
             <button onClick={handleClick}>click me</button>
-            <button onClick={() => handleClickAgain('mario')}>click me again</button>
+                <p>{name} is { age }</p>
+            {/* <button onClick={() => handleClickAgain('mario')}>click me again</button> */}
         </div>
     </div>
   )
