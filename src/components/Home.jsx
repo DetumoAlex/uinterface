2047728992cfec0d21fd0c2573a162016a44022c
detumo <adetumo@gmail.com>
2025-01-08 08:26:05 +0100

    custom hooks

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react"
 import BlogList from "./BlogList"
+import useFetch from "../useFetch"
 
 const Home = () => {
     // const [name, setName] = useState('alex')
@@ -10,38 +11,14 @@ const Home = () => {
     //     setName('detumo')
     //     setAge(30)
 
-    const [blogs, setBlogs] = useState(null)
-    const [isPending, setIsPending] = useState(true)
-    const [error, setError] =useState(null)
+   const { data: blogs, isPending, error} = useFetch('http://localhost:8000/blogs')
     
     // const handleDelete = (id) => {
     //     const newBlogs = blogs.filter(blog => blog.id !== id)
     //     setBlogs(newBlogs)
     // }
 
-    useEffect(() => {
-        setTimeout(() => {
-            fetch('http://localhost:8000/blogs')
-                .then(res => {
-                    // console.log(res);
-                    if(!res.ok)
-                        throw Error('could not fetch data')
-
-                    return res.json()
-                })
-                .then(data =>{
-                    setBlogs(data)
-                    setIsPending(false)
-                    setError(null)
-                })
-                .catch(err => {
-                    setIsPending(false)
-                    setError(err.message);
-                    
-                })
-        }, 1000)
-        
-    }, [])
+    
     // const handleClickAgain = () => {
     //     console.log(`hello $name`)
     // }
