ca4cfd51f11357d0b79dc38e9df842766de6afe7
detumo <adetumo@gmail.com>
2025-01-08 08:39:24 +0100

    React-Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
-import { useState } from 'react'
 import Home from './components/Home'
 import Navbar from './components/Navbar'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom/cjs/react-router-dom.min'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
+    <Router>
      <div className="App">
       <Navbar/>
       <div className="content">
-        <Home/>
+        <Switch>
+          <Route path="/">
+            <Home/>
+          </Route>
+        </Switch>
       </div>
      </div>
-    </>
+    </Router>
   )
 }
 
