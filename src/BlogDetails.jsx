f3c318b7aa2c9009c9ff6ddcb893d9a61e45c6d2
detumo <adetumo@gmail.com>
2025-01-09 08:28:52 +0100

    reusing custom=hooks

--- src/BlogDetails.jsx
+++ src/BlogDetails.jsx
@@ -1,11 +1,22 @@
 import { useParams } from "react-router-dom/cjs/react-router-dom.min";
+import useFetch from "./useFetch";
 
 const BlogDetails = () => {
     const { id } = useParams()
+    const {data: blog, error, isPending} = useFetch('http://localhost:8000/blogs/' + id)
 
     return ( 
         <div className="blog-details">
-            <h2>Blog details - {id}</h2>
+            {isPending && <div>Loading...</div>}
+            {error && <div>{error}</div>}
+            {blog && (
+                <article>
+                    <h2 className="my-5 text-pink-500">{blog.title}</h2>
+                    <p>Written by {blog.author}</p>
+                    <div className="my-5 mx-0">{blog.body}</div>
+                </article>
+            )}
+            
         </div>
      );
 }
