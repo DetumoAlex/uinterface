6bee50a782e72a164e801b8464088157b8b08f97
detumo <adetumo@gmail.com>
2025-01-09 20:47:50 +0100

    controlled inputs(forms)

--- src/Create.jsx
+++ src/Create.jsx
@@ -1,7 +1,37 @@
+import { useState } from "react";
+
 const Create = () => {
+    const [title, setTitle] = useState('')
+    const [body, setBody] = useState('')
+    const [author, setAuthor] = useState('mario')
+
     return (  
-        <div className="create">
-            <h1>Add new blogs</h1>
+        <div className="create max-w-[400px] my-0 mx-auto text-center">
+            <h2 className=" text-pink-400 text-xl mb-7 mt-8">Add new blogs</h2>
+            <form>
+                <label className="text-center block">Blog title:</label>
+                <input type="text" required className="w-full py-2 px-3 my-3 mx-0 border box-border block" 
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                />
+                <label className="text-center block">Blog body:</label>
+                <textarea required className="w-full py-2 px-3 my-3 mx-0 border box-border block"
+                    value={body}
+                    onChange={(e) => setBody(e.target.value)}
+                ></textarea>
+                <label className="text-center block">Blog author:</label>
+                <select className="w-full py-2 px-3 my-3 mx-0 border box-border block"
+                    value={author}
+                    onChange={(e) => setAuthor(e.target.value)}
+                >
+                    <option value="mario">mario</option>
+                    <option value="yoshi">yoshi</option>
+                </select>
+                <button className="bg-pink-300 text-white p-2 rounded-lg cursor-pointer">Add Blog</button>
+                <p>{ title} </p>
+                <p>{body}</p>
+                <p>{author}</p>
+            </form>
         </div>
     );
 }
