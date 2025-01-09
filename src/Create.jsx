66ca9d87a0b893bb9aa9f80be25f9aca167f7e64
detumo <adetumo@gmail.com>
2025-01-09 21:08:06 +0100

    submit events

--- src/Create.jsx
+++ src/Create.jsx
@@ -4,11 +4,28 @@ const Create = () => {
     const [title, setTitle] = useState('')
     const [body, setBody] = useState('')
     const [author, setAuthor] = useState('mario')
+    const [isPending, setIsPending] = useState(false)
+
+    const handleSubmit = (e) =>{
+        e.preventDefault()
+        const blog = {title, body, author}
+
+        setIsPending(true)
+
+        fetch( 'http://localhost:8000/blogs' ,{
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json'},
+            body: JSON.stringify(blog)
+        }).then(() => {
+            console.log('new blod added');
+            setIsPending(false)
+        })
+    }
 
     return (  
         <div className="create max-w-[400px] my-0 mx-auto text-center">
             <h2 className=" text-pink-400 text-xl mb-7 mt-8">Add new blogs</h2>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label className="text-center block">Blog title:</label>
                 <input type="text" required className="w-full py-2 px-3 my-3 mx-0 border box-border block" 
                     value={title}
@@ -27,10 +44,12 @@ const Create = () => {
                     <option value="mario">mario</option>
                     <option value="yoshi">yoshi</option>
                 </select>
-                <button className="bg-pink-300 text-white p-2 rounded-lg cursor-pointer">Add Blog</button>
+                {!isPending && <button className="bg-pink-300 text-white p-2 rounded-lg cursor-pointer">Add Blog</button>}
+                {isPending && <button className="bg-pink-300 text-white p-2 rounded-lg cursor-pointer">Adding Blog...</button>}
                 <p>{ title} </p>
                 <p>{body}</p>
                 <p>{author}</p>
+                
             </form>
         </div>
     );
